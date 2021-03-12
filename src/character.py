6072f1282988d44f35117358f8100b4665589e99
Mihaly Hegedus <hegedus.mihaly88@gmail.com>
2021-03-12 20:13:44 +0100

    Add race to character

--- src/character.py
+++ src/character.py
@@ -1,6 +1,10 @@
 class Character:
-    def __init__(self, name):
+    def __init__(self, name, race):
         self.name = name
+        self.race = race
 
     def getName(self):
         return self.name
+
+    def getRace(self):
+        return self.race
--- test_character.py
+++ test_character.py
@@ -1,5 +1,9 @@
 from .src import character
 
 def test_get_character_name():
-    fighter = character.Character("Herkules")
+    fighter = character.Character("Herkules", "Human")
     assert fighter.getName() == "Herkules"
+
+def test_get_character_race():
+    fighter = character.Character("Herkules", "Human")
+    assert fighter.getRace() == "Human"
